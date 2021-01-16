9c63d6ee51907384bd01a12bcb5ec6cf21ecb6e1
Billyoyo <billyoyo@hotmail.co.uk>
2021-01-16 22:24:57 +0000

    unsure class is non-null when validating

--- src/class.ts
+++ src/class.ts
@@ -13,7 +13,7 @@ export default function TClass<
   const templatearr = (templates as unknown) as NamedTemplate[];
 
   return {
-    valid: (o: any): o is any => typeof o === 'object' && templatearr.every(([key, t]) => t.valid(o[key])),
+    valid: (o: any): o is any => o && typeof o === 'object' && templatearr.every(([key, t]) => t.valid(o[key])),
     toModel: (o: any) => new cls(...templatearr.map(([key, t]) => t.toModel(o[key as keyof T]))),
     toTransit: (c: C) =>
       templatearr.reduce((obj: any, [key, t]) => ({ ...obj, [key]: t.toTransit((c as any)[key]) }), {}),
